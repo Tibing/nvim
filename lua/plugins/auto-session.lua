86b9e92cffc1259c53256bf1834202a026c99f26
Tibing <nik.poltoratsky@gmail.com>
2024-03-10 14:20:39 +0200

    feat: none-ls as formatter for lua

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,42 +1,42 @@
 -- Define a function to close Neo-tree
 local function close_neo_tree()
-  -- Utilizes the Neo-tree API to close all instances before saving the session
-  require("neo-tree.sources.manager").close_all()
+	-- Utilizes the Neo-tree API to close all instances before saving the session
+	require("neo-tree.sources.manager").close_all()
 end
 
 -- Define a function to open Neo-tree focused on the filesystem
 local function open_neo_tree()
-  -- Utilize the Neo-tree API to open the filesystem view
-  -- TODO: Open the Neo-tree in the current directory
-  require("neo-tree.sources.manager").show("filesystem")
+	-- Utilize the Neo-tree API to open the filesystem view
+	-- TODO: Open the Neo-tree in the current directory
+	require("neo-tree.sources.manager").show("filesystem")
 end
 
 return {
-  "rmagatti/auto-session",
-  config = function()
-    require("auto-session").setup({
+	"rmagatti/auto-session",
+	config = function()
+		require("auto-session").setup({
 
-      pre_restore_cmds = { close_neo_tree },
+			pre_restore_cmds = { close_neo_tree },
 
-      -- Register the pre-save hook to close Neo-tree
-      pre_save_cmds = { close_neo_tree },
+			-- Register the pre-save hook to close Neo-tree
+			pre_save_cmds = { close_neo_tree },
 
-      -- Register the post-restore hook to open Neo-tree
-      post_restore_cmds = { open_neo_tree },
+			-- Register the post-restore hook to open Neo-tree
+			post_restore_cmds = { open_neo_tree },
 
-      session_lens = {
-        -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
-        buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
-        load_on_setup = true,
-        theme_conf = { border = true },
-        previewer = false,
-      },
+			session_lens = {
+				-- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+				buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+				load_on_setup = true,
+				theme_conf = { border = true },
+				previewer = false,
+			},
 
-      -- Set mapping for searching a session.
-      vim.keymap.set("n", "<Leader>sl", require("auto-session.session-lens").search_session, {
-        noremap = true,
-        desc = "Show session list",
-      }),
-    })
-  end,
+			-- Set mapping for searching a session.
+			vim.keymap.set("n", "<Leader>sl", require("auto-session.session-lens").search_session, {
+				noremap = true,
+				desc = "Show session list",
+			}),
+		})
+	end,
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,21 +1,21 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function ()
-    require("neo-tree").setup({
-      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-      filesystem = {
-        bind_to_cwd = false,
-        follow_current_file = { enabled = true },
-        use_libuv_file_watcher = true,
-      },
-    })
-    vim.keymap.set('n', '<leader>n', ':Neotree filesystem toggle<CR>', {})
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+			open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+			filesystem = {
+				bind_to_cwd = false,
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true,
+			},
+		})
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle<CR>", {})
+	end,
 }

8f7f7120ec5b1ffa2812378e658f00a3f86763c2
Tibing <nik.poltoratsky@gmail.com>
2024-03-10 14:13:16 +0200

    feat: add auto session

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,6 +24,8 @@ return {
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
       -- go to implementation
       vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      -- show usages
+      vim.keymap.set('n', 'gu', "<cmd>Telescope lsp_references<cr>", { desc = "Show usages" })
       -- show documentation for function under cursor
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       -- show code actions selector

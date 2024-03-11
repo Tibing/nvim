90a423a1254b80bc104cb3869af1998628c4d367
Tibing <nik.poltoratsky@gmail.com>
2024-03-11 13:33:26 +0200

    feat: bufferline and remove buffers keymap

---
+++ lua/plugins/bufremove.lua
@@ -0,0 +1,24 @@
+local bufremove = function()
+  local bd = require("mini.bufremove").delete
+  if vim.bo.modified then
+    local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+    if choice == 1 then -- Yes
+      vim.cmd.write()
+      bd(0)
+    elseif choice == 2 then -- No
+      bd(0, true)
+    end
+  else
+    bd(0)
+  end
+end
+
+return {
+  "echasnovski/mini.bufremove",
+  version = "*",
+  config = function()
+    require("mini.bufremove").setup()
+
+    vim.keymap.set("n", "<leader>bd", bufremove, { desc = "Buf: delete" })
+  end,
+}

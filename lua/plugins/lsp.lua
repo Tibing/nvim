3577a9f013184a34fff611d5c140a77e94d5844e
Tibing <nik.poltoratsky@gmail.com>
2024-03-11 14:27:50 +0200

    feat: completion from lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,24 +1,35 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				insure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
 
 			-- go to definition
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "GOTO: definition" })

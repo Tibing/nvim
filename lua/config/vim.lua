653942703f0ada7be1a7f2b0ba43ae0ce7198348
Tibing <nik.poltoratsky@gmail.com>
2024-03-11 13:55:37 +0200

    fix: make lualine global

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -3,5 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.cmd("set laststatus=3")
 vim.g.mapleader = " "
 vim.opt.swapfile = false

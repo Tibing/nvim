3c41045ca0d9ac6f725101870dba831c1035537e
Tibing <nik.poltoratsky@gmail.com>
2024-03-11 20:46:32 +0200

    feat: add keymap to delete other buffers

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,26 +1,25 @@
 return {
 	"akinsho/bufferline.nvim",
-	event = "VeryLazy",
-	opts = {
-		options = {
-      -- stylua: ignore
-      close_command = function(n) require("mini.bufremove").delete(n, false) end,
-      -- stylua: ignore
-      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-			diagnostics = "nvim_lsp",
-			always_show_bufferline = false,
-			offsets = {
-				{
-					filetype = "neo-tree",
-					text = "Neo-tree",
-					highlight = "Directory",
-					text_align = "left",
+	config = function()
+		require("bufferline").setup({
+			options = {
+        -- stylua: ignore
+        close_command = function(n) require("mini.bufremove").delete(n, false) end,
+        -- stylua: ignore
+        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
 				},
 			},
-		},
-	},
-	config = function(_, opts)
-		require("bufferline").setup(opts)
+		})
+
 		-- Fix bufferline when restoring a session
 		vim.api.nvim_create_autocmd("BufAdd", {
 			callback = function()
@@ -29,5 +28,12 @@ return {
 				end)
 			end,
 		})
+
+		vim.keymap.set(
+			"n",
+			"<leader>bo",
+			"<Cmd>BufferLineCloseOthers<CR>",
+			{ desc = "Buf: delete others", silent = true }
+		)
 	end,
 }
